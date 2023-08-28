b78f4c4743b6aa124af122b6f650d3d2a268951c
Jarrod Swords <jarrod.swords@gmail.com>
2023-08-28 14:45:24 -0400

    add EnrollmentService.Register

---
+++ mocking/source/EnrollmentService.cs
@@ -0,0 +1,32 @@
+﻿namespace Mocking;
+
+public class EnrollmentService
+{
+    private readonly IDateTimeProvider _dateTimeProvider;
+
+    #region Creation
+
+    public EnrollmentService(IDateTimeProvider dateTimeProvider)
+    {
+        _dateTimeProvider = dateTimeProvider;
+    }
+
+    #endregion
+
+    #region Implementation
+
+    public Enrollment Register(Student student, Class @class)
+    {
+        @class.Add(student);
+        return CreateEnrollment(student, @class, _dateTimeProvider.Now);
+    }
+
+    #endregion
+
+    #region Static Interface
+
+    private static Enrollment CreateEnrollment(Student student, Class @class, DateTime enrolledOn) =>
+        new(@class.Id, student.Id, enrolledOn);
+
+    #endregion
+}
